b889bf07469cefbb2faee331793dc7536a56cd11
Gdansklol <swedenebomi@gmail.com>
2022-12-28 03:25:25 +0100

    added CD

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="title-row">
-        <h1 className="title">Welcome to Cruellas Recipies</h1>
+        <h1 className="title">Welcome to Blucrowns Recipies</h1>
       </div>
     </div>
   );
